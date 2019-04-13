5c39110b12aa482e910d1c0098a1a7e3bf7e6191
Zing Yang <zing9264@gmail.com>
2019-04-14 01:44:29 +0800

    part 2

--- evil-extension/page.js
+++ evil-extension/page.js
@@ -10,11 +10,64 @@ const MATCH_LIST = {
   'THEY\'RE': 'THERE'
 };
 
+
 function transformTextNodes(node) {
   // TODO(you): Implement this function! See HW spec for details.
+    let text=[];
+    let flag=0;
+    if(node.nodeType == Node.TEXT_NODE){
+        console.log('text:' + node.textContent);
+        for(let i in node.textContent.split(" ")){
+            for(var match of Object.keys(MATCH_LIST)){
+                if(node.textContent.split(" ")[i]== match+"\n" ||node.textContent.split(" ")[i]== match  ){
+                    flag=1;
+                    break;
+                }
+            }
+            if(flag===1){
+                text = text + MATCH_LIST[match]+' ';
+            }
+            else {
+                text = text + node.textContent.split(" ")[i]+' ';
+            }
+            flag=0;
+
+        }
+        console.log("unchange"+ node.textContent);
+        console.log("changed "+text);
+        node.textContent=text;
+    }else {
+        console.log( node.nodeName);
+    }
+    for (const child of node.childNodes) {
+        if(child==='STYLE' || child==='SCRIPT'){
+            return;
+        }
+        transformTextNodes(child);
+    }
 }
+
 
 transformTextNodes(document.body);
 
 // Log statement to test that the extension loaded properly.
 console.log('Evil extension loaded!');
+console.log('Extension updated');
+
+/*
+for(let i in node.textContent.split(" ")){
+    for(let match of Object.keys(MATCH_LIST)){
+        console.log("key:"+match+"   value"+MATCH_LIST[match]);
+        console.log(typeof match+"   value"+MATCH_LIST[match]);
+        console.log("str:"+node.textContent.split(" ")[i]);
+        console.log(typeof node.textContent.split(" ")[i]);
+        if(node.textContent.split(" ")[i]== match+"\n" ){
+            console.log("+++++");
+            node.textContent.split(" ")[i]=MATCH_LIST[match];
+            break;
+        }
+    }
+    text = text + node.textContent.split(" ")[i]+' ';
+}
+console.log(text);
+node.textContent=text;*/
